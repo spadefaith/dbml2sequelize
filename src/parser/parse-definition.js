9c570bc11ee2226bd690db2778a5b9b2daebfae0
Cedrick Campoto <cedrick.campoto@yahoo.com>
2024-10-05 17:16:18 +0800

    refactor

--- src/parser/parse-definition.js
+++ src/parser/parse-definition.js
@@ -1,22 +1,25 @@
-
 module.exports = function parseDefinition(tableDef, callback) {
-    const splitted = tableDef.split("\n");
-    const first = splitted[0];
-    const defs = splitted.slice(1, splitted.length - 1);
+  const splitted = tableDef.split("\n");
+  const first = splitted[0];
+  const defs = splitted.slice(1, splitted.length - 1);
 
-  
-    const content = defs.reduce((accu,line) => {
-      line = line.trim();
-      if (line) {
-        accu.push(line);
-      }
- 
-      return accu;
-    },[]);
+  let isOpen = false;
+  const content = defs.reduce((accu, line) => {
+    line = line.trim();
+    const isCodeBlock = line.includes("```");
+    if (!isCodeBlock) {
+      isOpen = !isOpen;
+    }
 
+    if (line && isOpen && !isCodeBlock) {
+      accu.push(line);
+    }
 
-    return callback({
-        content,
-        first
-    });
-  }
+    return accu;
+  }, []);
+
+  return callback({
+    content,
+    first,
+  });
+};
--- src/utils/grouping.js
+++ src/utils/grouping.js
@@ -1,36 +1,43 @@
 module.exports = function grouping(read) {
-    const splitted = read.split("\n");
-  
-    const definitions = [];
-    let defStr = [];
-    let openTagCount = 0;
-    for (let i = 0; i < splitted.length; i++) {
-      const line = splitted[i].trim();
-  
-      if (!line) {
-        continue;
-      }
-  
-      let openTagIndex = line.indexOf("{");
-      let closeTagIndex = line.indexOf("}");
-  
-      let isOpenTag = openTagIndex >= 0;
-      let isCloseTag = closeTagIndex >= 0;
-  
-      isOpenTag && (openTagCount += 1);
-      isCloseTag && (openTagCount -= 1);
-  
-      if (openTagCount) {
-        defStr.push(line);
-      }
-  
-      if (isCloseTag && !openTagCount) {
-        defStr.push("}");
-        definitions.push(defStr);
-  
-        defStr = [];
-      }
+  const splitted = read.split("\n");
+
+  const definitions = [];
+  let defStr = [];
+  let openTagCount = 0;
+  for (let i = 0; i < splitted.length; i++) {
+    const line = splitted[i].trim();
+
+    if (!line) {
+      continue;
     }
-  
-    return definitions;
+
+    let openTagIndex = line.indexOf("{");
+    let closeTagIndex = line.indexOf("}");
+
+    let isOpenTag = openTagIndex >= 0;
+    let isCloseTag = closeTagIndex >= 0;
+
+    isOpenTag && (openTagCount += 1);
+    isCloseTag && (openTagCount -= 1);
+
+    if (openTagCount) {
+      defStr.push(line);
+    }
+
+    if (isCloseTag && !openTagCount) {
+      defStr.push("}");
+      definitions.push(defStr);
+
+      defStr = [];
+    }
   }
+
+  /**
+   * skip Project definitions
+   */
+  const d = definitions.filter((def) => {
+    return def[0] && !def[0].includes("Project ");
+  });
+
+  return d;
+};
