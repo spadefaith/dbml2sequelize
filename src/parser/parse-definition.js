464c55a0d3e074761b3b393e102842c22a72cb3b
Cedrick Campoto <cedrick.campoto@yahoo.com>
2024-10-05 18:29:32 +0800

    update

--- src/parser/parse-definition.js
+++ src/parser/parse-definition.js
@@ -3,17 +3,10 @@ module.exports = function parseDefinition(tableDef, callback) {
   const first = splitted[0];
   const defs = splitted.slice(1, splitted.length - 1);
 
-  let isOpen = false;
   const content = defs.reduce((accu, line) => {
     line = line.trim();
-    const isCodeBlock = line.includes("```");
-    if (!isCodeBlock) {
-      isOpen = !isOpen;
-    }
 
-    if (line && isOpen && !isCodeBlock) {
-      accu.push(line);
-    }
+    line && accu.push(line);
 
     return accu;
   }, []);
